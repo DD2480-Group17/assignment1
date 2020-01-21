61b163767239fa34d9c24249471b35f7ff4661da
Edvin Ardö <ardo@kth.se>
2020-01-21 19:46:50 +0100

    Added tests for lic0

--- src/Decide.java
+++ src/Decide.java
@@ -7,14 +7,13 @@ public class Decide {
     Parameters parameters;
     LCM lcm;
     PUV puv;
-    boolean[] CMV = new boolean[15];
 
     /**
      * Launch Interceptor Condition 0
      * @return true if there exists at least one set of two consecutive data points that are a distance greater than
-     * the length, LENGTH1, apart.
+     * the length, length1, apart.
      */
-    private boolean lic0() {
+    boolean lic0() {
         for (int i = 1; i < points.size(); i++) {
             if (dist(points.get(i - 1), points.get(i)) > parameters.length1) {
                 return true;
@@ -29,7 +28,7 @@ public class Decide {
      * @param point2 the second point
      * @return the euclidian distance between two points
      */
-    private double dist(Point point1, Point point2) {
-        return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+    double dist(Point point1, Point point2) {
+        return Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2));
     }
 }
