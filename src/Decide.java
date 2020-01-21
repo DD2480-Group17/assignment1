879806bdf6f844b664df511eb1c100d1f98a9011
Edvin Ardö <ardo@kth.se>
2020-01-21 15:36:05 +0100

    added code skeleton

---
+++ src/Decide.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Decide {
+    int numPoints;
+    ArrayList<Point> points;
+    Parameters parameters;
+    LCM lcm;
+    PUV puv;
+}
---
+++ src/Parameters.java
@@ -0,0 +1,21 @@
+public class Parameters {
+    double length1;
+    double radius1;
+    double epsilon;
+    double area1;
+    int qPts;
+    int quads;
+    double dist;
+    int nPts;
+    int kPts;
+    int aPts;
+    int bPts;
+    int cPts;
+    int dPts;
+    int ePts;
+    int fPts;
+    int gPts;
+    double length2;
+    double radius2;
+    double area2;
+}
