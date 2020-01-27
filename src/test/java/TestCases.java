0d27eb150cd29a460f840c3dd689121317d1546c
Edvin Ardö <ardo@kth.se>
2020-01-27 20:23:10 +0100

    test/#20 added three main test cases

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,4 +1,173 @@
 public class TestCases {
+    public static final String mainTestCase1 = "5\n" +
+            "2.0 2.0\n" +
+            "1.0 1.0\n" +
+            "-1.0 1.0\n" +
+            "-1.0 -1.0\n" +
+            "1.0 -1.0\n" +
+            "1.0\n" +
+            "2.0\n" +
+            "0.78539816339\n" +
+            "1.0\n" +
+            "2\n" +
+            "3\n" +
+            "2.0\n" +
+            "3\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "2\n" +
+            "2.0\n" +
+            "1.0\n" +
+            "2.0\n" +
+            "ANDD ORR ORR ORR ANDD ANDD NOTUSED ORR ANDD ANDD ORR ANDD ANDD ANDD NOTUSED\n" +
+            "NOTUSED ORR ORR NOTUSED ORR NOTUSED NOTUSED ANDD NOTUSED ORR NOTUSED ORR ANDD ORR NOTUSED\n" +
+            "NOTUSED NOTUSED NOTUSED NOTUSED NOTUSED ANDD ANDD NOTUSED NOTUSED NOTUSED ANDD NOTUSED ORR ORR ANDD\n" +
+            "ANDD NOTUSED ANDD ORR ANDD ORR ORR ANDD NOTUSED ANDD ORR ANDD NOTUSED ANDD ORR\n" +
+            "ANDD ANDD NOTUSED NOTUSED ORR ANDD ANDD ANDD ANDD NOTUSED NOTUSED NOTUSED NOTUSED ORR NOTUSED\n" +
+            "ORR ORR NOTUSED NOTUSED ORR ORR ANDD ORR NOTUSED NOTUSED ANDD ANDD NOTUSED ANDD ORR\n" +
+            "ANDD ANDD ORR NOTUSED ANDD ANDD ANDD ANDD ORR NOTUSED NOTUSED ANDD NOTUSED ORR ORR\n" +
+            "ORR ANDD ORR ANDD NOTUSED ORR ORR ANDD ORR ORR ANDD NOTUSED ORR NOTUSED ANDD\n" +
+            "NOTUSED NOTUSED ANDD NOTUSED ORR ANDD NOTUSED NOTUSED NOTUSED ANDD ORR NOTUSED ORR ORR NOTUSED\n" +
+            "ORR ORR ORR NOTUSED ORR NOTUSED ORR ANDD NOTUSED NOTUSED NOTUSED ANDD ANDD ORR ORR\n" +
+            "NOTUSED ANDD ANDD ANDD ANDD ANDD ANDD ANDD ORR ORR ORR ORR ANDD ORR ANDD\n" +
+            "ANDD NOTUSED ANDD ANDD NOTUSED ANDD ORR ANDD NOTUSED NOTUSED NOTUSED ANDD ORR ORR NOTUSED\n" +
+            "ANDD ORR ANDD ORR ORR NOTUSED NOTUSED ORR ANDD ANDD ORR NOTUSED ORR NOTUSED ANDD\n" +
+            "NOTUSED NOTUSED ORR ORR ANDD ORR ORR NOTUSED NOTUSED ANDD ANDD NOTUSED NOTUSED NOTUSED ORR\n" +
+            "ORR NOTUSED ORR ANDD ORR ANDD NOTUSED NOTUSED NOTUSED ANDD ANDD NOTUSED NOTUSED NOTUSED NOTUSED\n" +
+            "true\n" +
+            "true\n" +
+            "false\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true";
+
+    public static final String mainTestCase2 = "3\n" +
+            "0.0 0.0\n" +
+            "5.0 0.0\n" +
+            "-5.0 0.0\n" +
+            "3.0\n" +
+            "5.0\n" +
+            "1.57079632679\n" +
+            "2.0\n" +
+            "2\n" +
+            "1\n" +
+            "2.0\n" +
+            "3\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1\n" +
+            "1.0\n" +
+            "1.0\n" +
+            "1.0\n" +
+            "NOTUSED ORR NOTUSED ORR ORR ANDD NOTUSED ORR NOTUSED ANDD ANDD ORR ANDD ORR NOTUSED\n" +
+            "ANDD ORR ORR NOTUSED ORR ANDD ORR NOTUSED NOTUSED ORR NOTUSED NOTUSED ANDD NOTUSED ORR\n" +
+            "ANDD ORR ORR ORR NOTUSED ORR ANDD NOTUSED ANDD ANDD ORR ANDD ORR NOTUSED ORR\n" +
+            "ORR ANDD NOTUSED NOTUSED ANDD NOTUSED ANDD ANDD ORR NOTUSED NOTUSED ORR ANDD NOTUSED ANDD\n" +
+            "ANDD ORR ORR ORR ANDD ORR NOTUSED NOTUSED ORR ORR ANDD NOTUSED ANDD NOTUSED ORR\n" +
+            "ORR NOTUSED ORR ORR ANDD ANDD ANDD ORR ORR NOTUSED ANDD ANDD ORR ANDD NOTUSED\n" +
+            "ORR NOTUSED NOTUSED ANDD ANDD ANDD ANDD NOTUSED ORR ORR ORR ANDD ORR NOTUSED ANDD\n" +
+            "ORR NOTUSED ANDD NOTUSED ANDD ORR ORR ORR NOTUSED ORR ANDD ORR NOTUSED NOTUSED ORR\n" +
+            "ANDD NOTUSED NOTUSED ANDD ANDD ORR ORR ORR NOTUSED NOTUSED NOTUSED ANDD NOTUSED ORR ANDD\n" +
+            "ANDD NOTUSED NOTUSED ANDD NOTUSED ANDD NOTUSED NOTUSED NOTUSED NOTUSED ANDD NOTUSED ANDD ORR NOTUSED\n" +
+            "ORR ANDD ANDD ANDD NOTUSED NOTUSED NOTUSED ORR NOTUSED NOTUSED NOTUSED NOTUSED NOTUSED ORR ANDD\n" +
+            "ANDD ANDD ANDD ANDD ANDD ORR ORR ANDD ORR NOTUSED ANDD NOTUSED ORR ORR NOTUSED\n" +
+            "ORR ORR NOTUSED ANDD NOTUSED NOTUSED NOTUSED NOTUSED ANDD ORR ORR NOTUSED ORR ANDD ORR\n" +
+            "ANDD NOTUSED NOTUSED ANDD ANDD ANDD ORR ANDD ORR ORR ANDD ORR ORR ORR ANDD\n" +
+            "ANDD ORR NOTUSED NOTUSED ANDD NOTUSED ORR NOTUSED NOTUSED ANDD ANDD NOTUSED NOTUSED ORR ORR\n" +
+            "true\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false\n" +
+            "false";
+
+
+    public static final String mainTestCase3 = "7\n" +
+            "0.0 0.0\n" +
+            "1.0 0.0\n" +
+            "1.0 1.0\n" +
+            "2.0 1.0\n" +
+            "2.0 2.0\n" +
+            "0.0 2.0\n" +
+            "0.0 0.0\n" +
+            "3.0\n" +
+            "3.0\n" +
+            "0.39269908169\n" +
+            "3.0\n" +
+            "1\n" +
+            "2\n" +
+            "5.0\n" +
+            "4\n" +
+            "2\n" +
+            "2\n" +
+            "2\n" +
+            "2\n" +
+            "2\n" +
+            "2\n" +
+            "2\n" +
+            "2\n" +
+            "3.0\n" +
+            "3.0\n" +
+            "3.0\n" +
+            "ANDD ANDD ANDD ORR ANDD ANDD ORR ORR ANDD ORR ORR NORUSED ANDD NORUSED ORR\n" +
+            "ORR ANDD NORUSED ANDD ORR NORUSED ANDD ANDD NORUSED ORR NORUSED NORUSED ORR ANDD ORR\n" +
+            "ANDD ORR ORR NORUSED ORR NORUSED ANDD ORR NORUSED ORR ORR ORR ANDD ORR NORUSED\n" +
+            "ORR NORUSED ANDD NORUSED ANDD ANDD NORUSED NORUSED ORR NORUSED ORR ORR ORR NORUSED NORUSED\n" +
+            "NORUSED ORR ORR ANDD NORUSED NORUSED NORUSED ANDD NORUSED ORR NORUSED ORR NORUSED ORR NORUSED\n" +
+            "NORUSED ANDD NORUSED ORR ANDD NORUSED NORUSED ANDD ORR NORUSED ANDD ORR ORR ORR ORR\n" +
+            "ORR ORR ANDD ANDD ANDD NORUSED NORUSED NORUSED ORR ANDD ORR ORR NORUSED ORR ORR\n" +
+            "ORR NORUSED ORR ANDD NORUSED ORR ANDD ORR ORR NORUSED NORUSED NORUSED ANDD ANDD ORR\n" +
+            "ORR ANDD ANDD ORR NORUSED ANDD ANDD ANDD ORR ORR NORUSED ANDD ORR ANDD ANDD\n" +
+            "ORR ANDD ANDD ORR NORUSED NORUSED ANDD ORR ANDD ORR ORR ORR ANDD ORR ORR\n" +
+            "ANDD ORR ANDD ANDD ANDD NORUSED NORUSED ANDD NORUSED ANDD ORR NORUSED ANDD ORR NORUSED\n" +
+            "NORUSED ORR NORUSED NORUSED NORUSED ORR NORUSED ORR ORR ORR ORR ORR ORR NORUSED ORR\n" +
+            "ANDD ANDD ANDD ORR ORR ORR ANDD NORUSED NORUSED NORUSED NORUSED ORR ANDD ORR NORUSED\n" +
+            "ORR ANDD NORUSED ORR ORR ORR ORR ORR ORR ANDD ANDD ORR ORR ANDD ANDD\n" +
+            "NORUSED ORR ORR ANDD NORUSED ANDD ANDD NORUSED NORUSED ANDD ANDD NORUSED ANDD NOTUSED ANDD\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true\n" +
+            "true";
+
     public static final String test1 = "4\n" +
             "0.0 0.0\n" +
             "0.0 1.0\n" +
