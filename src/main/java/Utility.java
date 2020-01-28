e212f16247b404608d1251c4e96ae0f2230f465b
Johanna <jii@kth.se>
2020-01-28 13:44:26 +0100

    refactor/#59 Changed some spelling mistake and added some documentation to the Utility class

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -2,12 +2,13 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class Utility {
+
     /**
-     * @param point1 point 1 used in smallest enclosing circle
-     * @param point2 point 2 used in smallest enclosing circle
-     * @param point3 point 3 used in smallest enclosing circle
-     * @param r      wanted radius of the smallest enclosing circle.
-     * @return true, if the three points can not be contained in a circle of radius r
+     * @param point1 point 1 used in smallest enclosing circle.
+     * @param point2 point 2 used in smallest enclosing circle.
+     * @param point3 point 3 used in smallest enclosing circle.
+     * @param r wanted radius of the smallest enclosing circle.
+     * @return true, if the three points can not be contained in a circle of radius r.
      */
     public static boolean canNotContainPoints(Point2D point1, Point2D point2, Point2D point3, double r) {
         ArrayList<Point> tempPoints = new ArrayList<>();
@@ -23,9 +24,9 @@ public class Utility {
      * Angle is calculated counter-clockwise from vector point1 -> point0 towards
      * vector point1 -> point2.
      *
-     * @param point0 one point
-     * @param point1 vertex
-     * @param point2 another point
+     * @param point0 one point.
+     * @param point1 vertex.
+     * @param point2 another point.
      * @return angle (in radians) formed by point0, point1 and point2. 0 <= angle <
      * 2pi. Angle is calculated counter-clockwise from vector point1 ->
      * point0 towards vector point1 -> point2.
@@ -50,9 +51,9 @@ public class Utility {
     /**
      * trianlgeArea calculates the area formed by 3 data points
      *
-     * @param point0
-     * @param point1
-     * @param point2
+     * @param point0 one vertex of a triangle
+     * @param point1 second vertex of a triangle
+     * @param point2 third vertex of a triangle
      * @return the area of the tringle formed by point0, point1 and point2
      */
     public static double triangleArea(Point2D.Double point0, Point2D.Double point1, Point2D.Double point2) {
@@ -63,11 +64,11 @@ public class Utility {
     }
 
     /**
-     * Calculates the distance between a single point and a line with Heron's formula.
+     * Calculates the distance between a single point and a line.
      *
      * @param startPoint the start point of the line
-     * @param point      the point
-     * @param endPoint   the end point of the line
+     * @param point the point
+     * @param endPoint the end point of the line
      * @return the distance between the single point and the line
      */
     public static double distPointLine(Point2D.Double startPoint, Point2D.Double point, Point2D.Double endPoint) {
@@ -75,6 +76,7 @@ public class Utility {
         double pToStart = dist(point, startPoint);
         double pToEnd = dist(point, endPoint);
 
+        //calcAngle() is calculating the angle clockwise -> the angle between startPoint and Point will therefore be 2*Pi - angle
         if (((2 * Math.PI) - (calcAngle(startPoint, endPoint, point))) >= (Math.PI / 2) || (calcAngle(endPoint, startPoint, point) >= (Math.PI / 2))) {
 
             if (pToStart > pToEnd) {
@@ -105,6 +107,4 @@ public class Utility {
         return Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2));
 
     }
-
-
 }
